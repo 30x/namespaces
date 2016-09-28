d2c846080e7523ed2db20a3dea77232248fed9b7
Martin Nally <mnally@apigee.com>
2016-09-27 19:48:13 -0700

    Ganyoize

--- namespaces.js
+++ namespaces.js
@@ -1,7 +1,7 @@
-'use strict';
-var http = require('http');
-var url = require('url');
-var lib = require('http-helper-functions');
+'use strict'
+var http = require('http')
+var url = require('url')
+var lib = require('http-helper-functions')
 
 function verifyNamespace(ns) {
   var name = ns.name
@@ -15,24 +15,23 @@ function verifyNamespace(ns) {
 }
 
 function createNamespace(req, res, ns) {
-  var user = lib.getUser(req);
+  var user = lib.getUser(req)
   if (user == null) {
-    lib.unauthorized(req, res);
+    lib.unauthorized(req, res)
   } else { 
-    var err = verifyNamespace(ns);
-    if (err !== null) {
-      lib.badRequest(res, err);
-    } else {
-      var permissions = ns.permissions;
-      if (permissions !== undefined) {
-        delete ns.permissions;
-      }
-      var selfURL = makeSelfURL(req, ns.name);
+    var err = verifyNamespace(ns)
+    if (err !== null)
+      lib.badRequest(res, err)
+    else {
+      var permissions = ns.permissions
+      if (permissions !== undefined)
+        delete ns.permissions
+      var selfURL = makeSelfURL(req, ns.name)
       lib.createPermissonsFor(req, res, selfURL, permissions, function(permissionsURL, permissions){
         addCalculatedNamespaceProperties(req, ns, selfURL)
         lib.created(req, res, ns, selfURL)
-      });
-        // We are not going to store any information about a namespace, since we can recover its name from its permissions document 
+      })
+      // We are not going to store any information about a namespace, since we can recover its name from its permissions document 
     }
   }
 }
@@ -42,8 +41,8 @@ function makeSelfURL(req, key) {
 }
 
 function addCalculatedNamespaceProperties(req, map, selfURL) {
-  map.self = selfURL; 
-  map._permissions = `protocol://authority/permissions?${map.self}`;
+  map.self = selfURL
+  map._permissions = `protocol://authority/permissions?${map.self}`
 }
 
 function getNamespace(req, res, id) {
@@ -52,7 +51,7 @@ function getNamespace(req, res, id) {
     var namespace = {isA: 'Namespace', name: req.url.split('/').slice(-1)[0]}
     addCalculatedNamespaceProperties(req, namespace, selfURL)
     lib.found(req, res, namespace)
-  });
+  })
 }
 
 function deleteNamespace(req, res, id) {
@@ -66,39 +65,37 @@ function deleteNamespace(req, res, id) {
         var namespace = {isA: 'Namespace', name: req.url.split('/').slice(-1)[0]}
         addCalculatedNamespaceProperties(req, namespace, selfURL)
         lib.found(req, res, namespace)
-      } else {
+      } else
         getClientResponseBody(clientRes, function(body) {
           var err = {statusCode: clientRes.statusCode, msg: `failed to create permissions for ${resourceURL} statusCode ${clientRes.statusCode} message ${body}`}
           internalError(serverRes, err)
         })
-      }
     })
   })
 }
 
 function requestHandler(req, res) {
-  if (req.url == '/namespaces') {
-    if (req.method == 'POST') {
-      lib.getServerPostObject(req, res, createNamespace);
-    } else { 
-      lib.methodNotAllowed(req, res, ['POST']);
-    }
-  } else {
-    var req_url = url.parse(req.url);
+  if (req.url == '/namespaces')
+    if (req.method == 'POST')
+      lib.getServerPostObject(req, res, createNamespace)
+    else 
+      lib.methodNotAllowed(req, res, ['POST'])
+  else {
+    var req_url = url.parse(req.url)
     if (req_url.pathname.lastIndexOf('/namespaces;', 0) > -1 && req_url.search == null) {
-      var id = req_url.pathname.substring('/namespaces;'.length);
+      var id = req_url.pathname.substring('/namespaces;'.length)
       if (req.method == 'GET') 
-        getNamespace(req, res, id);
+        getNamespace(req, res, id)
       else if (req.method == 'DELETE') 
-        deleteNamespace(req, res, id);
+        deleteNamespace(req, res, id)
       else
-        lib.methodNotAllowed(req, res, ['GET', 'DELETE']);
+        lib.methodNotAllowed(req, res, ['GET', 'DELETE'])
     } else 
       lib.notFound(req, res)
   }
 }
 
-var port = process.env.PORT;
+var port = process.env.PORT
 http.createServer(requestHandler).listen(port, function() {
-  console.log(`server is listening on ${port}`);
-});
+  console.log(`server is listening on ${port}`)
+})
